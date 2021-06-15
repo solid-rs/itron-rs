87127dfaa777208d5465be4e4f829c250e93ba68
Tomoaki Kawada <kawada@kmckk.co.jp>
2021-06-15 15:59:51 +0900

    chore: raise the target compiler version
    `extended_key_value_attributes` will be stabilized in Rust 1.54
    according to <https://caniuse.rs/features/extended_key_value_attrs>.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 #![no_std]
 #![cfg_attr(feature = "doc_cfg", feature(doc_cfg))]
 #![cfg_attr(feature = "nightly", feature(decl_macro))]
-// Can't be filtered by `cfg_attr`. I'm sure <https://github.com/rust-lang/rust/pull/83366>
-// is going to be stabilized very soon.]
-#![feature(extended_key_value_attributes)]
 #![doc = include_str!("lib.md")]
 #![deny(unsafe_op_in_unsafe_fn)]
 
