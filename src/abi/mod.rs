174bd7793b92317e78bdd039430027893434d47e
Tomoaki Kawada <kawada@kmckk.co.jp>
2021-06-15 16:18:38 +0900

    doc: display the active Cargo features in the documentation

--- src/abi/mod.rs
+++ src/abi/mod.rs
@@ -4,6 +4,14 @@
 //! provide different items with conflicting names. This module's documentation
 //! might not be useful unless you ran `cargo doc` with an appropriate kernel
 //! selection.
+//!
+//! <i>This documentation has been built with the following Cargo features:
+// Make sure the commas are inserted at the right places!
+#![cfg_attr(feature = "asp3", doc = "`asp3`")]
+#![cfg_attr(feature = "none", doc = "`none`")]
+#![cfg_attr(feature = "dcre", doc = ", `dcre`")]
+#![cfg_attr(feature = "rstr_task", doc = ", `rstr_task`")]
+//! </i>
 #![allow(non_camel_case_types)]
 #![allow(unused_imports)]
 mod error;
